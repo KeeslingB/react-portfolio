332b1f20b46224ce845ff3e6389a73136941daf2
unknown <Breke64@gmail.com>
2024-06-05 21:32:50 -0400

    removed extra navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Navigation() {
   return (
     <Navbar className="nav">
