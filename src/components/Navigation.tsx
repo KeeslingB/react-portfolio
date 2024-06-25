4c5008eb1a2dc939f326c6c01b1b19285a8ccb66
unknown <Breke64@gmail.com>
2024-06-25 11:46:30 -0400

    added icons to navigation bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,16 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faBarsProgress,
+  faAddressCard,
+  faFile,
+} from "@fortawesome/free-solid-svg-icons";
 function Navigation() {
   return (
     <div id="nav">
       <a className="links" href="#home">
+        <FontAwesomeIcon className="icons" icon={faHouse} />
         Home
       </a>
       <a className="links" href="#projects">
+        <FontAwesomeIcon className="icons" icon={faBarsProgress} />
         Projects
       </a>
       <a className="links" href="#contact">
+        <FontAwesomeIcon className="icons" icon={faAddressCard} />
         Contact
       </a>
       <a className="links" href="#resume">
+        <FontAwesomeIcon icon={faFile} />
         Resume
       </a>
     </div>
