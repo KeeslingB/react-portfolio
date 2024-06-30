6714ec6a4b1a17fb8f92814241cf231c7e250dd9
unknown <Breke64@gmail.com>
2024-06-30 10:58:44 -0400

    added not found component for use later

---
+++ src/seeds.js
@@ -0,0 +1,10 @@
+import mvc from "./assets/images/mvc-armory.png";
+
+export const projects = [
+  {
+    img: mvc,
+    title: "MVC-Armory",
+    text: "MVC-Armory Text",
+    href: "https://github.com/KeeslingB/mvc-armory",
+  },
+];
