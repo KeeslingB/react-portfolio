baa3d7cc3600b18a7f816d7fd803e6bbf3e637f0
unknown <Breke64@gmail.com>
2024-11-19 14:23:01 -0500

    adding routes within app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/home" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
       </Routes>
     </>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,20 +3,18 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/resume">Resume</Link>
-        </li>
-      </ul>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        <Link to="/projects">Projects</Link>
+      </div>
+      <div>
+        <Link to="/contact">Contact</Link>
+      </div>
+      <div>
+        <Link to="/resume">Resume</Link>
+      </div>
     </>
   );
 }
