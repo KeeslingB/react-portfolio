704f8ee1b095663a98f94d6c67f30862181c2af4
unknown <Breke64@gmail.com>
2024-11-23 19:22:49 -0500

    adding to css files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,9 @@ import "./Header.css";
 import Nav from "./Nav";
 export default function Header() {
   return (
-    <>
-      <div className="header">
-        <h2 className="title">Brendan Keesling</h2>
-        <Nav />
-      </div>
-    </>
+    <div className="header">
+      <h2 className="title">Brendan Keesling</h2>
+      <Nav />
+    </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="home-page">
       <h1>Home Page</h1>
     </div>
   );
